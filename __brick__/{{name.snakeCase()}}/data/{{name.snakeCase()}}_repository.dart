e68cf3f584ae8ff5d59f4b21585f1dd42fd7cbcd
Daniel Waraksa <d.waraksa@futuremind.com>
2023-03-17 13:43:24 +0100

    fix: delete methods

--- __brick__/{{name.snakeCase()}}/data/{{name.snakeCase()}}_repository.dart
+++ __brick__/{{name.snakeCase()}}/data/{{name.snakeCase()}}_repository.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
 import '../{{name.snakeCase()}}.dart';
@@ -6,28 +5,9 @@ import '../{{name.snakeCase()}}.dart';
 
 @injectable
 class {{name.pascalCase()}}Repository {
-  const {{name.pascalCase()}}Repository(this._dataSource);
+  const {{name.pascalCase()}}Repository(this._{{name.camelCase()}}DataSource);
 
-  final {{name.pascalCase()}}DataSource _dataSource;
-
-  Future<Either<Exception, {{name.pascalCase()}}Model>> get{{name.pascalCase()}}() async {
-    // TODO: implement get{{name.pascalCase()}}
-  }
-
-  //post 
-  Future<Either<Exception, {{name.pascalCase()}}Model>> post{{name.pascalCase()}}({required {{name.pascalCase()}}Model {{name.camelCase()}}}) async {
-    // TODO: implement post{{name.pascalCase()}}
-  }
-
-  //put
-  Future<Either<Exception, {{name.pascalCase()}}Model>> put{{name.pascalCase()}}({required {{name.pascalCase()}}Model {{name.camelCase()}}}) async {
-    // TODO: implement put{{name.pascalCase()}}
-  }
-
-  //delete
-  Future<Either<Exception, {{name.pascalCase()}}Model>> delete{{name.pascalCase()}}({required {{name.pascalCase()}}Model {{name.camelCase()}}}) async {
-    // TODO: implement delete{{name.pascalCase()}}
-  }
+  final {{name.pascalCase()}}DataSource _{{name.pascalCase()}}DataSource;
 
 }
 
